eedc25be7645689c7b74cf36bcbebc766f86498c
andresws12 <andreshernandez12.1995@gmail.com>
2023-11-05 22:27:12 +0100

    Add headers security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
     "nuxt-delay-hydration",
     "nuxt-simple-sitemap",
     "nuxt-schema-org",
+    'nuxt-security'
   ],
   site: {
     url: "https://andres-hernandez.onrender.com/",
