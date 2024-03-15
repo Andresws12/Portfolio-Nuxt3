698d7fc8e3f5ee00e2759630ef9ace937fcf6f72
andresws12 <andreshernandez12.1995@gmail.com>
2024-03-15 22:22:18 +0100

    try another  provider image

--- components/home/HomeDescription.vue
+++ components/home/HomeDescription.vue
@@ -7,7 +7,7 @@
   >
     <NuxtImg
       class="home-description__image"
-      src="/img/andres-front-programador.webp"
+      src="https://res.cloudinary.com/dx8q5jh0g/image/upload/v1710537658/andres-front-programador_mbprmh.webp"
       :alt="$t('common.images.dataBodyImage')"
       sizes="220px sm:180px md:240px lg:250px xl:350px"
       densities="1x 2x"
